46b88adee0793b6e1ab8da37e3154634c7d40f86
Nath <nathinabob@gmail.com>
2026-04-28 11:35:59 +0100

    feat: give routes names and link 'pray' button to route

--- lib/router.dart
+++ lib/router.dart
@@ -10,6 +10,8 @@ import 'screens/pray_screen.dart';
 import 'screens/reminders_screen.dart';
 import 'screens/settings_screen.dart';
 
+enum AppRoute { home, pray, peopleGroups, reminders }
+
 final GlobalKey<NavigatorState> _rootNavigatorKey = GlobalKey<NavigatorState>(
   debugLabel: 'root',
 );
@@ -25,6 +27,7 @@ final GoRouter appRouter = GoRouter(
         StatefulShellBranch(
           routes: [
             GoRoute(
+              name: AppRoute.home.name,
               path: '/home',
               builder: (_, _) => const HomeScreen(),
             ),
@@ -33,6 +36,7 @@ final GoRouter appRouter = GoRouter(
         StatefulShellBranch(
           routes: [
             GoRoute(
+              name: AppRoute.pray.name,
               path: '/pray',
               builder: (_, _) => const PrayScreen(),
             ),
@@ -41,6 +45,7 @@ final GoRouter appRouter = GoRouter(
         StatefulShellBranch(
           routes: [
             GoRoute(
+              name: AppRoute.peopleGroups.name,
               path: '/people-groups',
               builder: (_, _) => const PeopleGroupsScreen(),
             ),
@@ -49,6 +54,7 @@ final GoRouter appRouter = GoRouter(
         StatefulShellBranch(
           routes: [
             GoRoute(
+              name: AppRoute.reminders.name,
               path: '/reminders',
               builder: (_, _) => const RemindersScreen(),
             ),
@@ -57,17 +63,20 @@ final GoRouter appRouter = GoRouter(
       ],
     ),
     GoRoute(
+      name: 'people-group-details',
       path: '/people-groups/:slug',
       parentNavigatorKey: _rootNavigatorKey,
       builder: (_, state) =>
           PeopleGroupDetailsScreen(slug: state.pathParameters['slug']),
     ),
     GoRoute(
+      name: 'settings',
       path: '/settings',
       parentNavigatorKey: _rootNavigatorKey,
       builder: (_, _) => const SettingsScreen(),
     ),
     GoRoute(
+      name: 'gallery',
       path: '/gallery',
       parentNavigatorKey: _rootNavigatorKey,
       builder: (_, _) => const GalleryScreen(),
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -19,7 +19,7 @@ class HomeScreen extends StatelessWidget {
   }
 
   void _openPray(BuildContext context) {
-    context.push('/pray');
+    context.goNamed('pray');
   }
 
   Widget _peopleGroupCardOrCTA() {
